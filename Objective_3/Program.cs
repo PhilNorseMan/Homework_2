5bda270c57320b6535730aeb5e4f9f0291fd7ecf
Philipp K <philippkargapoloff@gmail.com>
2022-10-22 23:37:19 +0300

    added objective 3

---
+++ Objective_3/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+void DayCount(int dayNum)
+{
+    if(dayNum > 7) 
+    {
+        Console.WriteLine("НЕТ ТАКОГО ДНЯ НЕДЕЛИ!=)");
+    }
+    else
+    {
+        if(dayNum > 5)
+        {
+            Console.WriteLine("Выходной день");
+        }
+        else
+        {
+            Console.WriteLine("Рабочий день");
+        }
+    }
+}
+
+Console.WriteLine("Введите номер дня недели:");
+int dayNum = int.Parse(Console.ReadLine()!);
+
+DayCount(dayNum);
