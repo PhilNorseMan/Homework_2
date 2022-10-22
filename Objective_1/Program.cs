6959f987bd1620fffa94b1ea3200aa7bcb1d2fcf
Philipp K <philippkargapoloff@gmail.com>
2022-10-22 22:38:28 +0300

    added objective 1

---
+++ Objective_1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа. Через строку решать нельзя.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine()!);
+
+bool ThreeDigitNum(int num)
+{
+    return 99 < num && num < 1000;
+}
+
+if(ThreeDigitNum(num))
+{
+    int secondNum = num / 10 % 10;
+    Console.WriteLine("Вторая цифра введенного числа = " + secondNum);
+}
+else
+{
+    Console.WriteLine("ВВЕДЕНО НЕ ТРЕХЗНАЧНОЕ ЧИСЛО!!!");
+}
